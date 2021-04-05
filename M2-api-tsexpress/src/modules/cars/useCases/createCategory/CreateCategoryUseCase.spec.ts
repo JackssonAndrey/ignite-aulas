0e21bbde66ce977c7dc3b04e37a5c4dd31bb6588
Andrey Araujo <jaab.andrey@gmail.com>
2021-04-05 08:43:19 -0300

    chapter IV: create a first test for create category use case

---
+++ M2-api-tsexpress/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,41 @@
+import { AppError } from '../../../../errors/AppError';
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import CreateCategoryUseCase from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'Category Test',
+      description: 'Categpry description test',
+    };
+    await createCategoryUseCase.excute(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty('id');
+  });
+
+  it('should be able to create a new category with name exists', async () => {
+    expect(async () => {
+      const category = {
+        name: 'Category Test',
+        description: 'Categpry description test',
+      };
+
+      await createCategoryUseCase.excute(category);
+      await createCategoryUseCase.excute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
